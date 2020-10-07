fa8eff0308363f849b1a04844e2c746cd7d3bcbe
joeland1 <55666119+joeland1@users.noreply.github.com>
2020-10-07 17:13:27 -0400

    can get ip

--- setup.sh
+++ setup.sh
@@ -1,12 +1,42 @@
 # setup master container
 # setup.sh [ip_address] [masternode_key]
-MN_IP=${1-null}
-MN_KEY=${2-null}
+MN_IP=${1-}
+MN_KEY=${2-}
 
-function test()
+RED='\033[0;31m'
+NC='\033[0m'
+NEXT='\033[0;34m'
+
+#ifconfig.me
+#ifconfig.co
+#icanhazip.com
+SECOND_LINK='ifconfig.me'
+
+function get_ip()
 {
-  echo $MN_IP
-  echo $MN_KEY
+  MN_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+  MN_IP=
+  if [ -z "$MN_IP" ]; then
+      echo -e "${RED}[ERROR]: Could not get external ip from opendns.com${NC}"
+      echo -e "[INFO]: Trying to get ip from ${SECOND_LINK}"
+
+      MN_IP=$(curl -s ${SECOND_LINK})
+      MN_IP=
+
+      if [ -z "$MN_IP" ]; then
+        echo -e "${RED}[ERROR]: Could not get external ip from ${SECOND_LINK} ${NC}"
+        echo -e "${NEXT}[NEXT]: Please use another link or manually enter in an ip address${NC}"
+
+        exit 1
+      fi
+
+  fi
+  echo "[INFO]: IP Address Aquired -> ${MN_IP}"
 }
 
-test
+function getmnkey()
+{
+  echo "generate mn key"
+}
+
+get_ip
