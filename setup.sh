f9f72e212fe36f99d9f3572afb3f5f7aee73d92a
joeland1 <55666119+joeland1@users.noreply.github.com>
2020-10-07 10:19:59 -0400

    optional variables work

--- setup.sh
+++ setup.sh
@@ -1,2 +1,12 @@
-#!/bin/bash
-#setup.sh ip_address masternode_key -> all perams optinoal
+# setup master container
+# setup.sh [ip_address] [masternode_key]
+MN_IP=${1-null}
+MN_KEY=${2-null}
+
+function test()
+{
+  echo $MN_IP
+  echo $MN_KEY
+}
+
+test
