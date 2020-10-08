220161effe1f67cd2557991e54dd549c13e5b30c
joeland1 <55666119+joeland1@users.noreply.github.com>
2020-10-08 11:10:30 -0400

    foundation for optional peram + other fixes

--- setup.sh
+++ setup.sh
@@ -1,17 +1,24 @@
 # setup master container
-# setup.sh [ip_address] [masternode_key]
-MN_IP=${1-}
-MN_KEY=${2-}
+# setup.sh --ip [ip addpress] --key [masternode_key]
+MN_IP=${1-} #-> ip not needed because already dl from docker
+MN_KEY=${1-}
 
 RED='\033[0;31m'
-NC='\033[0m'
+GREEN='\033[0;32m'
 NEXT='\033[0;34m'
+
+NC='\033[0m'
+
+CLI_NAME='dogecash-cli'
+DAEMON_NAME='dogecashd'
+DAEMON_CONFIG=''
+
+
 
 #ifconfig.me
 #ifconfig.co
 #icanhazip.com
 SECOND_LINK='ifconfig.me'
-
 function get_ip()
 {
   MN_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
@@ -24,19 +31,28 @@ function get_ip()
       MN_IP=$(curl -s ${SECOND_LINK})
 
       if [ -z "$MN_IP" ]; then
-        echo -e "${RED}[ERROR]: Could not get external ip from ${SECOND_LINK} ${NC}"
+        echo -e "${RED}[ERROR]: Could not get external ip from ${SECOND_LINK}${NC}"
         echo -e "${NEXT}[NEXT]: Please use another link or manually enter in an ip address${NC}"
 
         exit 1
       fi
-
   fi
-  echo "[INFO]: IP Address Aquired -> ${MN_IP}"
+  echo -e "${GREEN}[INFO]: IP Address Aquired -> ${MN_IP}${NC}"
 }
 
 function getmnkey()
 {
-  echo "generate mn key"
+  echo "x"
 }
 
-get_ip
+for arg in "$@"
+  do
+    case $arg in
+        "--ip" )
+           echo "ip"
+           echo "ip2"
+           ;;
+        "--key" )
+           echo "key";;
+    esac
+  done
