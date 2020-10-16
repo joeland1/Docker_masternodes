aa32caed4ae3a4cf64d979ea354db6148856ac81
joeland1 <55666119+joeland1@users.noreply.github.com>
2020-10-16 17:51:17 -0400

    generates info

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,23 @@
-FROM ubuntu:xenial-20200916
+FROM ubuntu:18.04
+ENV SHELL /bin/bash
+CMD ["/bin/bash"]
 
 ARG COIN="dogecash"
 
 RUN mkdir -p /home/$COIN
+RUN mkdir -p /home/$COIN/.$COIN
 WORKDIR /home/$COIN
 
 COPY setup.sh .
+COPY dogecashd .
+COPY dogecash-cli .
+
+RUN chmod +x .
+
+ADD . /usr/local/bin
+
+RUN useradd -ms /bin/bash $COIN
+
+USER $COIN
 
 EXPOSE 56740
--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
+
 # setup master container
 # setup.sh --ip [ip addpress] --key [masternode_key]
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NEXT='\033[0;34m'
@@ -10,11 +13,15 @@ CLI_NAME=$COIN_NAME"-cli"
 DAEMON_NAME=$COIN_NAME"d"
 
 HOME_PATH="/home/$COIN_NAME"
+CONF_NAME='dogecash.conf'
 
-DAEMON_CONFIG_LAUNCH="-daemon -datadir=$HOME_PATH/.$COIN_NAME"
+DAEMON_CONFIG_LAUNCH="-daemon -datadir=$HOME_PATH/.$COIN_NAME -conf=$HOME_PATH/.$COIN_NAME/$CONF_NAME"
 
 HAS_IP=false
 HAS_KEY=false
+
+MN_IP=
+MN_KEY=
 
 #ifconfig.me
 #ifconfig.co
@@ -38,36 +45,44 @@ function get_ip()
         exit 1
       fi
   fi
+  HAS_IP=true
+  echo "externalip=$MN_IP" >> $HOME_PATH/.$COIN_NAME/$CONF_NAME
   echo -e "$GREEN[INFO]: IP address aquired -> $MN_IP$NC"
 }
 
 function getmnkey()
 {
   echo -e "[INFO]: Generating $COIN_NAME masternode key"
-  exec $HOME_PATH/$DAEMON_NAME $DAEMON_CONFIG_LAUNCH
-  MN_KEY=$HOME_PATH/$CLI_NAME getblockcount
-  while [$MN_KEY -eq -1]; do
+  ./$DAEMON_NAME $DAEMON_CONFIG_LAUNCH
+  source "$HOME_PATH/.$COIN_NAME/$CONF_NAME"
+  echo $rpcuser
+  echo $rpcpassword
+  sleep 3
+  MN_KEY=$(./$CLI_NAME -rpcuser=$rpcuser -rpcpassword=$rpcpassword getblockcount)
+  echo "mnkey="$MN_KEY
+
+  while [ "$MN_KEY" = "-1" ] || [ -z "$MN_KEY" ]; do
     sleep 1
-    MN_KEY=$HOME_PATH/$CLI_NAME getblockcount
+    MN_KEY=$(./$CLI_NAME -rpcuser=$rpcuser -rpcpassword=$rpcpassword getblockcount)
   done
 
-  MN_KEY=$HOME_PATH/$CLI_NAME createmasternodekey
-  $HOME_PATH/$CLI_NAME stop
-  echo -e "$GREEN[INFO]: Masternode key aquired -> $MN_IP$NC"
+  MN_KEY=$(./$CLI_NAME -rpcuser=$rpcuser -rpcpassword=$rpcpassword createmasternodekey)
+  ./$CLI_NAME -rpcuser=$rpcuser -rpcpassword=$rpcpassword stop
+  HAS_KEY=true
+  echo -e "$GREEN[INFO]: Masternode key aquired -> $MN_KEY$NC"
 }
 
 function write_conf()
 {
-
+    echo "server=1" > $HOME_PATH/.$COIN_NAME/$CONF_NAME
+    echo "rpcuser=user"$(cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '') >> $HOME_PATH/.$COIN_NAME/$CONF_NAME
+    echo "rpcpassword="$(cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 20 ; echo '') >> $HOME_PATH/.$COIN_NAME/$CONF_NAME
 }
 
 if [ $# -gt 4 ]; then
   echo "too many peram"
   exit 1
 fi
-
-MN_IP=
-MN_KEY=
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -76,13 +91,29 @@ while [ "$1" != "" ]; do
         exit 1
         ;;
       -i|--ip)
+        if [ $HAS_IP = true ]; then
+          echo "You cannot input 2 ip"
+          exit 1
+        fi
         MN_IP=$2
+        HAS_IP=true
         ;;
       -k|--key)
+        if [ $HAS_KEY = true ]; then
+          echo "You cannot input 2 mn keys"
+          exit 1
+        fi
         MN_KEY=$2
+        HAS_KEY=true
         ;;
       *)
     esac
     shift
     shift
 done
+
+apt-get update
+apt-get install dnsutils
+write_conf
+getmnkey
+get_ip
